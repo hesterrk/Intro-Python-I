abb2408e1e8813b5b97170d623087151ccfdcf88
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-03 16:07:40 +0100

    corrected primes stretch

--- src/16_prime.py
+++ src/16_prime.py
@@ -1,7 +1,5 @@
 import sys
 
-import sys
-
 
 if len(sys.argv) == 2:
     num = int(sys.argv[1])
@@ -9,13 +7,17 @@ if len(sys.argv) == 2:
         print('Not Valid!')
 
     else:
+        is_prime = True
         for x in range(2, num):
             if num % x == 0:
-                print('this is not a prime')
+                is_prime = False
                 break
-            elif num % x != 0:
-                print('this is a prime')
-                break
+
+        if is_prime:
+            print('The number is Prime')
+
+        else:
+            print('Its not prime')
 
 
 else:
@@ -25,6 +27,26 @@ else:
 # Logic --> Given a number n, print all primes smaller than or equal to n. It is also given that n is a small number
 # using for loop
 
+# def sieveOfErat(num):
+#     # prime is an array of type boolean with True values in it
+#     prime = [True for i in range(n + 1)]
+#     p = 2
+#     while (p * p <= n):
+#       # If it remain unchanged it is prime
+#         if (prime[p] == True):
+#             # updating all the multiples
+#             for i in range(p * 2, n + 1, p):
+#                 prime[i] = False
+#         p += 1
+#     prime[0] = False
+#     prime[1] = False
+#     # Print
+#     for p in range(n + 1):
+#         if prime[p]:
+#             print(p, end=" ")
 
-# int(sys.argv[1]) % 2 == 0:
-#     print('Its a prime')
+
+# if __name__ == '__main__':
+#     n = int(sys.argv[1])
+#     print("The prime numbers smaller than or equal to", n, "is")
+#     sieveOfErat(n)
